ae90c7d3dc0fe45c7127f18bdcc42a2e58cbc7e8
Svyatoslav <meier.sviatoslav@yandex.ru>
2022-06-11 21:55:40 +0300

    программа максимальное из 3 чисел

---
+++ hw_c#/004_/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+
+int a, b, c;
+Console.WriteLine("Введите три числа : ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine("{0} наибольшее число ", max);
